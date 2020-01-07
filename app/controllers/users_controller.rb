24cde37b1c5bdf40dded2ace22b2b473a0dc0c09
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-07 22:26:37 +0900

    Add Items CRUD

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: :show
+
   def show
+    @items = @user.items.recent
+    #@stocked_items = @user.stocked_items.recent
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :comments, dependent: :destroy
+
+  validates :title, :content, presence: true
+
+
+  scope :recent, -> { order(updated_at: :desc) }
+
 end
