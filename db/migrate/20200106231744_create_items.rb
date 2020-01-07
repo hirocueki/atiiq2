2dc415831ebf6a5d5adcc6012b6b4941755fd0d5
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-07 17:06:20 +0900

    Run db:migrate Add HomesController

--- db/migrate/20200106231744_create_items.rb
+++ db/migrate/20200106231744_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.string :title
-      t.text :content
-      t.user :references
+      t.string :title, null: false, default: ""
+      t.text :content, null: false, default: ""
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20200106231858_create_comments.rb
+++ db/migrate/20200106231858_create_comments.rb
@@ -1,9 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      t.string :content
-      t.user :references
-      t.item :references
+      t.string :content, null: false, default: ""
+      t.references :user, foreign_key: true
+      t.references :item, foreign_key: true
 
       t.timestamps
     end
