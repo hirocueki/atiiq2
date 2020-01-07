4d1a68334f5b1bb6d5bbddf9c1f10ffd0a2c4d56
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-07 18:16:52 +0900

    Add Bootstrap configuration

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # atiiq2
+
+Rails6 webpackでBS4を使う手順
+https://qiita.com/whitia/items/34de15b72946f7f3b773
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  get 'homes/index'
   devise_for :users
 
   root 'homes#index'
+
+  resources :users, :expect => [:index] do
+    resources :items do
+      resources :comments
+    end
+  end
 end
