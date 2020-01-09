d48d5eed78bfe0ecd802d6a3787f2e94626bc690
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-09 12:40:01 +0900

    dd model stocks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_06_231858) do
+ActiveRecord::Schema.define(version: 2020_01_09_033417) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2020_01_06_231858) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
+  create_table "stocks", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["item_id"], name: "index_stocks_on_item_id"
+    t.index ["user_id", "item_id"], name: "index_stocks_on_user_id_and_item_id", unique: true
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -49,4 +59,6 @@ ActiveRecord::Schema.define(version: 2020_01_06_231858) do
   add_foreign_key "comments", "items"
   add_foreign_key "comments", "users"
   add_foreign_key "items", "users"
+  add_foreign_key "stocks", "items"
+  add_foreign_key "stocks", "users"
 end
