a850c14c91d818242623d41f6a5ec6f427a1fc98
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-09 18:32:56 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
-  get 'items/index'
   devise_for :users
 
   root 'homes#index'
 
-  resources :users, :expect => [:index] do
+  resources :users, expect: :index do
     resources :items, expect: :index do
       resources :comments, only: :create
-
       resources :stocks, only: %i[create destroy]
     end
   end
