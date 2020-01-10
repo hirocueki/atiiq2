1e9a36af4a0b4226994580bc83407781489f8972
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-10 10:39:42 +0900

    Add Stock item, Follow/Un follow users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,26 @@
 class UsersController < ApplicationController
   before_action :set_user, only: :show
 
+  def index
+    #@users = User.page(params[:page]).order(:name)
+    @users = User.all.order(:email)
+  end
+
   def show
     @items = @user.items.recent
-    #@stocked_items = @user.stocked_items.recent
+    @stocked_items = @user.stocked_items.recent
+  end
+
+  def following
+    @user = User.find(params[:id])
+    @users = @user.following.order(:email)
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers.order(:email)
+    render 'show_follow'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,17 @@ Rails.application.routes.draw do
 
   root 'homes#index'
 
-  resources :users, expect: :index do
+  resources :users, only: %i[index show] do
+    member do
+      get :following, :followers
+    end
     resources :items, expect: :index do
       resources :comments, only: :create
       resources :stocks, only: %i[create destroy]
     end
   end
 
+  resource :tagfeed, only: :show
+  resource :timeline, only: :show
   resources :relationships, only: %i[create destroy]
 end
