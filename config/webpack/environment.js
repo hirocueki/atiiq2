e7e18ead414e8e4a2b387e834b38349696011363
Hiroshi Ueki <hirocueki@gmail.com>
2020-01-13 12:40:43 +0900

    fix

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,7 @@ const {environment} = require('@rails/webpacker')
 
 const webpack = require('webpack')
 
+// use Bootstrap4
 environment.plugins.prepend('Provide',
   new webpack.ProvidePlugin({
     $: 'jquery/src/jquery',
